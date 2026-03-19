5c59c3f794b4b32c5df42b43eda8950463cad55d
Kazleen June B. Caballero <kazleenjunecaballero@gmail.com>
2026-03-19 16:50:27 +0800

    update on mongoose

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static('.'));
 
 // 1. Connect to MongoDB
 // Replace the string below with your actual MongoDB connection string
-const mongoURI = "mongodb+srv://kazleen:gj5Je4qWPg7YP94n@cluster0.edipnmh.mongodb.net/?appName=Cluster0"; 
+const mongoURI = "mongodb+srv://kazleen:gj5Je4QWPg7YP94n@cluster0.cdipnmh.mongodb.net/?appName=Cluster0";
 mongoose.connect(mongoURI)
     .then(() => console.log("Connected to NEU Library Database"))
     .catch(err => console.error("Database connection error:", err));
